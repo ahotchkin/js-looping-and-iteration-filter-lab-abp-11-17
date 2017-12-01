a49a327c20cbd6a3202b8dd26fbabe0952ddecaa
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-01 02:46:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Code your solution in this file
 function findMatching (drivers, name){
-  drivers.filter(function(driver())){
+  let matchingDrivers = drivers.filter(function(driver())){
     if (name === driver){
       return true
     }else{
       return false
     }
     }
+    return matchingDrivers;
   }
